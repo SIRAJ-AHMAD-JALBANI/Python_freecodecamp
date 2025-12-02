c324b663b146ca40b63d25437911e6dcb0580f11
SIRAJ-AHMAD-JALBANI <s4495942@gmail.com>
2025-12-02 09:59:05 +0500

    Rpg character in python

---
+++ Rpg_character.py
@@ -0,0 +1,33 @@
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(name,strength,intelligence,charisma):
+    if not isinstance(name, str):
+        return "The character name should be a string"
+
+    if len(name) > 10:
+        return "The character name is too long"
+
+    if " " in name:
+        return "The character name should not contain spaces"
+
+    if not(isinstance(strength, int) and isinstance(intelligence, int) and isinstance(charisma, int)):
+        return "All stats should be integers"
+    
+    if (strength < 1) or (intelligence < 1) or (charisma < 1) :
+        return "All stats should be no less than 1" 
+    if (strength > 4) or (intelligence > 4) or (charisma > 4) :
+        return "All stats should be no more than 4" 
+    
+    if strength + intelligence + charisma != 7:
+        return "The character should start with 7 points"
+    
+    def make_bar(para):
+        return full_dot * para + empty_dot * (10 - para)
+    
+    output = (f"{name}\n"
+        f"STR {make_bar(strength)}\n"
+        f"INT {make_bar(intelligence)}\n"
+        f"CHA {make_bar(charisma)}")
+    
+    return output
